0eb516bd79d5ce6ffab84d0bd15482311cb93b6f
unknown <sc805036@gmail.com>
2022-12-30 08:28:34 -0500

    Commit de creacion de proyecto PHP_Basico

---
+++ PHP/Dic30_2022.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxis PHP y sintaxis de clases</title>
+</head>
+<body>
+    <h3>30 de diciembre del 2022</h3>
+    <h1>SINTAXIS EN PHP Y SINYAXIS DE CLASES</h1>
+</body>
+</html>
