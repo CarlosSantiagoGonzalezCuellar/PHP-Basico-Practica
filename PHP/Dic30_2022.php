b8ce83abe5f44e488c1d37af08760010d742a4d6
unknown <sc805036@gmail.com>
2022-12-30 13:20:32 -0500

    Ejercicios actualizados hechos con clases y metodos

--- PHP/Dic30_2022.php
+++ PHP/Dic30_2022.php
@@ -15,196 +15,367 @@
     <?php
     //-----------------Variables-----------------
     echo "<h2>VARIABLES:</h2>";
-    $texto = "Bienvenido!!";
-    $numEntero = 30;
-    $numDecimal = 4.5;
-    $valorBoolean = true;
+    class operacionesBasicas
+    {
+        public $num1;
+        public $num2;
 
-    $suma = $numDecimal + $numEntero;
-    $resta = $numDecimal - $numEntero;
-    $multiplicacion = $numDecimal * $numEntero;
-    $division = $numDecimal / $numEntero;
+        public function __construct($num1, $num2)
+        {
+            $this->num1 = $num1;
+            $this->num2 = $num2;
+        }
 
-    print $texto . "<br>";
-    echo "La suma de los valores es: " . $suma . "<br>";
-    echo "La resta de los valores es: " . $resta . "<br>";
-    echo "La multiplicacion de los valores es: " . $multiplicacion . "<br>";
-    echo "La division de los valores es: " . $division . "<br>";
-    var_dump($valorBoolean);
+        public function suma()
+        {
+            return $this->num1 + $this->num2;
+        }
 
-    echo "<br>";
-    echo "<br>";
+        public function resta()
+        {
+            return $this->num1 - $this->num2;
+        }
+
+        public function multiplicacion()
+        {
+            return $this->num1 * $this->num2;
+        }
+
+        public function division()
+        {
+            return $this->num1 / $this->num2;
+        }
+    }
+
+    $operacion = new operacionesBasicas(10, 20);
+    echo $operacion->suma() . "<br>";
+    echo $operacion->resta() . "<br>";
+    echo $operacion->multiplicacion() . "<br>";
+    echo $operacion->division() . "<br><br>";
+
 
     //-----------------Strings-----------------
     echo "<h2>STRINGS:</h2>";
-    $text = "Hola, buenos dias!!";
+    class funcionesTexto
+    {
+        public $texto;
 
-    echo strlen($text) . "<br>";
-    echo str_word_count($text) . "<br>";
-    echo strrev($text) . "<br>";
+        public function __construct($texto)
+        {
+            $this->texto = $texto;
+        }
 
-    echo "<br>";
-    echo "<br>";
+        public function cantidadLetras()
+        {
+            return strlen($this->texto);
+        }
+
+        public function cantidadPalabras()
+        {
+            return str_word_count($this->texto);
+        }
+
+        public function textoReverso()
+        {
+            return strrev($this->texto);
+        }
+    }
+
+    $texto = new funcionesTexto("Hola, buenas tardes!!");
+    echo $texto->cantidadLetras() . "<br>";
+    echo $texto->cantidadPalabras() . "<br>";
+    echo $texto->textoReverso() . "<br><br>";
+
 
     //-----------------Math-----------------
     echo "<h2>FUNCION MATH:</h2>";
-    echo (pi()) . "<br>";
-    echo (min(150, 45, 2, 800, -4)) . "<br>";
-    echo (max(-8, 10, 147, 6, 78)) . "<br>";
-    echo (abs(-14.7)) . "<br>";
-    echo (sqrt(50)) . "<br>";
-    echo (rand(1, 100)) . "<br>";
+    class funcionesMath
+    {
+        public $numero;
 
-    echo "<br>";
-    echo "<br>";
+        public function __construct($numero)
+        {
+            $this->numero = $numero;
+        }
+
+        public function operacionPi()
+        {
+            return $this->numero[1] * pi();
+        }
+
+        public function numeroMenor()
+        {
+            return min($this->numero);
+        }
+
+        public function numeroMayor()
+        {
+            return max($this->numero);
+        }
+
+        public function valorAbsoluto()
+        {
+            return abs($this->numero[0]);
+        }
+
+        public function raiz()
+        {
+            return sqrt($this->numero[2]);
+        }
+
+        public function numeroRandom()
+        {
+            return rand(0, $this->numero[4]);
+        }
+    }
+
+    $numero1 = new funcionesMath(array(-8, 10, 147, 6, 78));
+    echo $numero1->operacionPi() . "<br>";
+    echo $numero1->numeroMenor() . "<br>";
+    echo $numero1->numeroMayor() . "<br>";
+    echo $numero1->valorAbsoluto() . "<br>";
+    echo $numero1->raiz() . "<br>";
+    echo $numero1->numeroRandom() . "<br><br>";
+
 
     //-----------------Constantes-----------------
     echo "<h2>CONSTANTES:</h2>";
-    define("Mensaje", "El dia de hoy empiezo a practicar");
-    define("carros", [
+    class constantes
+    {
+        public $constante;
+
+        public function __construct($constante)
+        {
+            $this->constante = $constante;
+        }
+
+        public function primeroLista()
+        {
+            return $this->constante . carros[0];
+        }
+    }
+
+    $constante = new constantes(define("carros", [
         "Kia",
         "BMW",
         "Mercedes"
-    ]);
-    echo carros[1] . "<br>";
-    echo Mensaje . "<br>";
+    ]));
+    echo $constante->primeroLista() . "<br><br>";
 
-    echo "<br>";
-    echo "<br>";
 
     //-----------------Arreglos-----------------
     echo "<h2>ARREGLOS:</h2>";
-    $colores = array("Verde", "Azul", "Amarillo");
-    $nombres = array("Persona1" => "Santiago", "Persona2" => "Camilo", "Persona3" => "Luisa");
-    $numeros = array(45, 50, 2, 17, 27);
+    class arreglos
+    {
+        public $nombres;
+        public $numeros;
 
-    echo var_dump($colores) . "<br>";
-    echo count($nombres) . "<br>";
-    echo $colores[2] . "<br>";
-    echo $nombres["Persona1"] . "<br>";
+        public function __construct($nombres, $numeros)
+        {
+            $this->nombres = $nombres;
+            $this->numeros = $numeros;
+        }
 
-    echo "<br>";
-    echo "<br>";
+        public function tipoVariable()
+        {
+            return var_dump($this->nombres);
+        }
+
+        public function longitudArreglo()
+        {
+            return count($this->nombres);
+        }
+
+        public function llamadoIndice()
+        {
+            return $this->nombres["Persona1"];
+        }
+    }
+
+    $arreglos = new arreglos(
+        array("Persona1" => "Santiago", "Persona2" => "Camilo", "Persona3" => "Luisa"),
+        array(45, 50, 2, 17, 27)
+    );
+    echo $arreglos->tipoVariable() . "<br>";
+    echo $arreglos->longitudArreglo() . "<br>";
+    echo $arreglos->llamadoIndice() . "<br><br>";
+
 
     //-----------------Condicionales-----------------
     echo "<h2>CONDICIONALES:</h2>";
-    $edad = rand(0, 100);
-    echo $edad . "<br>";
-    if ($edad >= 18) {
-        echo "Es mayor de edad <br>";
-    } else {
-        echo "Es menor de edad <br>";
+    class condicionales
+    {
+        public $edad;
+        public $rango;
+
+        public function __construct($edad, $rango)
+        {
+            $this->edad = $edad;
+            $this->rango = $rango;
+        }
+
+        public function mayorMenorEdad()
+        {
+            echo $this->edad . "<br>";
+            if ($this->edad >= 18) {
+                return "Es mayor de edad";
+            } else {
+                return "Es menor de edad";
+            }
+        }
+
+        public function rangoPerteneciente()
+        {
+            if ($this->rango == "A") {
+                return "Su rango es alto";
+            } elseif ($this->rango == "B") {
+                return "Su rango es medio";
+            } else {
+                return "Su rango es bajo";
+            }
+        }
     }
 
-    $rango = "B";
-    if ($rango == "A") {
-        echo "Su rango es alto <br>";
-    } elseif ($rango == "B") {
-        echo "Su rango es medio <br>";
-    } else {
-        echo "Su rango es bajo <br>";
-    }
+    $condicionales = new condicionales(rand(0, 100), "A");
+    echo $condicionales->mayorMenorEdad() . "<br>";
+    echo $condicionales->rangoPerteneciente() . "<br><br>";
 
-    $categoria = ($edad >= 18) ? "Mayor" : "Menor";
-    echo $categoria . "<br>";
-
-    echo "<br>";
-    echo "<br>";
 
     //-----------------Switch-----------------
     echo "<h2>SWITCH:</h2>";
-    $mes = 2;
-    switch ($mes) {
-        case 1:
-            echo "Enero <br>";
-            break;
-        case 2:
-            echo "Febrero <br>";
-            break;
-        case 3:
-            echo "Marzo <br>";
-            break;
-        case 4:
-            echo "Abril <br>";
-            break;
-        case 5:
-            echo "Mayo <br>";
-            break;
+    class funcionSwitch
+    {
+        public $mes;
 
-        default:
-            echo "Mes inexistente <br>";
-            break;
+        public function __construct($mes)
+        {
+            $this->mes = $mes;
+        }
+
+        public function mayorMenorEdad()
+        {
+            switch ($this->mes) {
+                case 1:
+                    return "Enero";
+                    break;
+                case 2:
+                    return "Febrero";
+                    break;
+                case 3:
+                    return "Marzo";
+                    break;
+                case 4:
+                    return "Abril";
+                    break;
+                case 5:
+                    return "Mayo";
+                    break;
+
+                default:
+                    return "Mes inexistente <br>";
+                    break;
+            }
+        }
     }
 
-    echo "<br>";
-    echo "<br>";
+    $switch = new funcionSwitch(2);
+    echo $switch->mayorMenorEdad() . "<br><br>";
+
 
     //-----------------Ciclos-----------------
     echo "<h2>CICLOS:</h2>";
-    echo "<p>While...</p>";
-    $i = 1;
-    while ($i <= 7) {
-        echo "Numero: $i <br>";
-        $i++;
+    class ciclos
+    {
+        public $limite;
+        public $paises;
+        public $ciudades;
+
+        public function __construct($limite, $paises, $ciudades)
+        {
+            $this->limite = $limite;
+            $this->paises = $paises;
+            $this->ciudades = $ciudades;
+        }
+
+        public function numerosWhile()
+        {
+            echo "<p>While...</p>";
+            $i = 1;
+            while ($i <= $this->limite) {
+                echo "Numero: $i <br>";
+                $i++;
+            }
+        }
+
+        public function numerosDoWhile()
+        {
+            echo "<p>Do While...</p>";
+            $x = 1;
+            do {
+                echo "Numero: $x <br>";
+                $x++;
+            } while ($x <= $this->limite);
+        }
+
+        public function numerosFor()
+        {
+            echo "<p>For...</p>";
+            for ($j = 1; $j <= $this->limite; $j++) {
+                echo "Numero: $j <br>";
+            }
+        }
+
+        public function contenidoArregloFor()
+        {
+            $cantPaises = count($this->paises);
+            for ($i = 0; $i < $cantPaises; $i++) {
+                echo $this->paises[$i];
+                echo "<br>";
+            }
+        }
+
+        public function contenidoArregloForEach()
+        {
+            echo "<p>Foreach...</p>";
+            foreach ($this->ciudades as $i => $ciudad) {
+                echo "<li>$i - $ciudad</li>";
+            }
+        }
     }
 
-    echo "<br>";
-    echo "<br>";
+    $ciclos = new ciclos(
+        7,
+        array("Colombia", "Brasil", "Argentina", "Peru"),
+        array("Neiva", "Medellin", "Bogota", "Cali")
+    );
+    echo $ciclos->numerosWhile() . "<br>";
+    echo $ciclos->numerosDoWhile() . "<br>";
+    echo $ciclos->numerosFor() . "<br>";
+    echo $ciclos->contenidoArregloFor() . "<br>";
+    echo $ciclos->contenidoArregloForEach() . "<br><br>";
 
-    echo "<p>Do While...</p>";
-    $x = 1;
-    do {
-        echo "Numero: $x <br>";
-        $x++;
-    } while ($x <= 5);
-
-    echo "<br>";
-    echo "<br>";
-
-    echo "<p>For...</p>";
-    for ($j = 1; $j <= 4; $j++) {
-        echo "Numero: $j <br>";
-    }
-
-    echo "<br>";
-
-    $paises = array("Colombia", "Brasil", "Argentina", "Peru");
-    $cantPaises = count($paises);
-    for ($i = 0; $i < $cantPaises; $i++) {
-        echo $paises[$i];
-        echo "<br>";
-    }
-
-    echo "<br>";
-    echo "<br>";
-
-    echo "<p>Foreach...</p>";
-    $ciudades = array("Neiva", "Medellin", "Bogota", "Cali");
-    foreach ($ciudades as $i => $ciudad) {
-        echo "<li>$i - $ciudad</li><br>";
-    }
-
-    echo "<br>";
-    echo "<br>";
 
     //-----------------Funciones-----------------
     echo "<h2>FUNCIONES:</h2>";
-    function nombreEdad($nombre, $edad)
+    class funciones
     {
-        echo "Bienvenido $nombre, su edad es $edad años <br>";
+        public $nombre;
+        public $edad;
+
+        public function __construct($nombre, $edad)
+        {
+            $this->nombre = $nombre;
+            $this->edad = $edad;
+        }
+
+        public function nombreEdad()
+        {
+            return "Bienvenido " . $this->nombre . ", su edad es " . $this->edad . " años";
+        }
     }
 
-    nombreEdad("Santiago", 19);
-
-    function suma($num1, $num2)
-    {
-        return $num1 + $num2;
-    }
-
-    echo suma(12, 28);
-
-    echo "<br>";
-    echo "<br>";
+    $funcion = new funciones("Santiago", 19);
+    echo $funcion->nombreEdad() . "<br><br>";
 
     //-----------------Objetos-----------------
     echo "<h2>OBJETOS:</h2>";
@@ -224,10 +395,9 @@
     }
 
     $persona1 = new Persona("Santiago", 19);
-    echo $persona1->saludo();
-    echo "<br>";
+    echo $persona1->saludo() . "<br>";
     $persona1 = new Persona("Luisa", "21");
-    echo $persona1->saludo();
+    echo $persona1->saludo() . "<br><br>";
 
     ?>
 </body>
