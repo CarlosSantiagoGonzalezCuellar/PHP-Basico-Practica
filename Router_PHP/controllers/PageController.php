e0420782a86fb3cfb7726951c17e4944b9425b4c
unknown <sc805036@gmail.com>
2023-01-02 16:18:21 -0500

    Manejo de router en PHP 02/01/2023

---
+++ Router_PHP/controllers/PageController.php
@@ -0,0 +1,25 @@
+<?php
+class PageController
+{
+    public function home()
+    {
+        echo 'Home';
+    }
+    public function listar()
+    {
+        echo 'Listar';
+    }
+    public function modificar()
+    {
+        echo 'Modificar';
+    }
+
+    public function nuevo()
+    {
+        echo 'Nuevo';
+    }
+    public function eliminar()
+    {
+        echo 'Eliminar';
+    }
+}
